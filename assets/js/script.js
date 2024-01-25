08e1e1f0bed9fe9deb439c8c3c7f17bfce702173
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-25 15:27:15 +0000

    Implement division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,7 +125,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
+    let lh = operand1*operand2;
+    document.getElementById("operand1").textContent = lh;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = '/';
 }
